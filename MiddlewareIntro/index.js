3de37f92a36be44aa4d6353e57895ec028f33250
jli <jinyan_li@yahoo.com>
2023-01-05 21:53:16 -0800

    express custom error handlers

--- MiddlewareIntro/index.js
+++ MiddlewareIntro/index.js
@@ -1,6 +1,9 @@
+const { application } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+
+const AppError = require("./AppError");
 
 // use morgan middleware on every request - logs each request
 app.use(morgan("tiny"))
@@ -17,11 +20,13 @@ app.use("/dogs", (req, res, next) => {
 
 
 const verifyPassword = (req, res, next) => {
-    const { password } =req.query;
+    const { password } = req.query;
     if (password === "chickennugget") {
         next();
     }
-    res.send("SORRY YOU NEED A PASSWORD!")
+    throw new AppError("password required!", 401);
+    //res.send("SORRY YOU NEED A PASSWORD!")
+    // throw new AppError ("Password required!", 400)
 }
 
 // next() would execute the next matching middleware or route handler
@@ -44,6 +49,10 @@ app.get("/", (req, res) => {
     res.send("HOME PAGE!")
 })
 
+app.get("/error", (req, res) => {
+    chicken.fly()
+})
+
 app.get("/dogs", (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send("WOOF WOOF!")
@@ -54,9 +63,27 @@ app.get("/secret", verifyPassword, (req, res) => {
     res.send("MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone");
 })
 
+app.get("/admin", (req, res) => {
+    throw new AppError("You are not an Admin", 403)
+})
+
 // handle an undefined route
 app.use((req, res) => {
     res.status(404).send("NOT FOUND!")
+})
+
+// app.use((err, req, res, next) => {
+//     console.log("*********************************")
+//     console.log("**************ERROR**************")
+//     console.log("*********************************")
+//     // res.status(500).send("Oh boy! We got an error!")
+//     console.log(err)
+//     next(err)
+// })
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong" } = err;
+    res.status(status).send(message)
 })
 
 app.listen(3000, () => {
